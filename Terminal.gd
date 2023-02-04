d96ae7fa03711849c9a7b230bb4032677f75e991
roushk <34784335+roushk@users.noreply.github.com>
2023-02-04 11:51:07 -0800

    Changed space to use specific space sound and changed max size to 3
    Updated Terminal to scale better.
    Update to use theme

--- Terminal.gd
+++ Terminal.gd
@@ -3,7 +3,7 @@ extends LineEdit
 var click1 = preload("res://click1.wav")
 var click2 = preload("res://click2.wav")
 var click3 = preload("res://click3.wav")
-var click4 = preload("res://click4.wav")
+var clickSpace = preload("res://click4.wav")
 
 var rng = RandomNumberGenerator.new()
 
@@ -20,22 +20,18 @@ func _process_input(input_text):
 	#parse command
 	
 func _keypress(input_text):
-	rng.randomize()
-	var i = rng.randi_range(0, 3)
 	var click
-	match i:
-		0:
-			click = click1
-		1:
-			click = click2
-		2:
-			click = click3
-		3:
-			var j = rng.randi_range(0, 3)
-			if j == 3:
-				click = click4
-			else:
+	if input_text.length() > 1 && input_text[input_text.length() - 1] != " " :
+		rng.randomize()
+		match rng.randi_range(0, 2):
+			0:
 				click = click1
+			1:
+				click = click2
+			2:
+				click = click3
+	else:
+		click = clickSpace
 	$"../KeypressSoundEmitter".stream = click
 	$"../KeypressSoundEmitter".pitch_scale = 0.94
 	$"../KeypressSoundEmitter".volume_db = -12
